117c2304b205013274141e90f1c1c7e10e0da2f7
sams <santyago.lp@gmail.com>
2020-06-23 22:36:50 -0300

    clean

--- src/com/sams/Main.java
+++ src/com/sams/Main.java
@@ -1,13 +1,22 @@
 package com.sams;
 
 import com.sams.memento.Editor;
+import com.sams.memento.History;
 
 public class Main {
     public static void main(String[] args) {
         var editor = new Editor();
+        var history = new History();
+
         editor.setContent("a");
+        history.push(editor.createState());
+
         editor.setContent("b");
+        history.push(editor.createState());
+
         editor.setContent("c");
-        editor.undo();
+        editor.restore(history.pop());
+
+        System.out.println(editor.getContent());
     }
 }
--- src/com/sams/memento/Editor.java
+++ src/com/sams/memento/Editor.java
@@ -3,6 +3,14 @@ package com.sams.memento;
 public class Editor {
     private String content;
 
+    public EditorState createState() {
+        return new EditorState(content);
+    }
+
+    public void restore(EditorState state) {
+        content = state.getContent();
+    }
+
     public String getContent() {
         return content;
     }
